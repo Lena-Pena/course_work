eeeb143b8e6add53c989f511bdc802e976065564
viktor.livantsov@outlook.com <viktor.livantsov@outlook.com>
2020-11-29 22:34:39 +0300

    Add error handling in report blueprint

--- report/__init__.py
+++ report/__init__.py
@@ -2,7 +2,7 @@ from flask import Blueprint, render_template, request, current_app, redirect
 
 from auth import check_auth, get_current_role
 from utils import make_dict_list_from_rows
-from utils.UseDatabase import UseDatabase
+from utils.UseDatabase import UseDatabase, DBConnectionError, DBCredentialError, DBBaseError, DBSQLError
 from utils.decorators import allow_roles, allow_methods
 
 
@@ -28,20 +28,30 @@ def report_blueprint_root():
     current_role = get_current_role()
     year = request.form.get('year')
 
-    with UseDatabase(current_app.config['db'][current_role]) as cursor:
-        call_staff_turnover_query = f'call kursach.staff_turnover(\'{year}\')'
-        get_staff_turnover_records_count_query = f'select COUNT(*) as count from staff_turnover where st_year = {year}'
-        get_staff_turnover_records_query = f'select * from staff_turnover where st_year = {year}'
+    try:
+        with UseDatabase(current_app.config['db'][current_role]) as cursor:
+            call_staff_turnover_query = f'call kursach.staff_turnover(\'{year}\')'
+            get_staff_turnover_records_count_query = f'select COUNT(*) as count from staff_turnover where st_year = {year}'
+            get_staff_turnover_records_query = f'select * from staff_turnover where st_year = {year}'
 
-        cursor.execute(get_staff_turnover_records_count_query)
-        result = make_dict_list_from_rows(cursor)
+            cursor.execute(get_staff_turnover_records_count_query)
+            result = make_dict_list_from_rows(cursor)
 
-        if not result[0]['count']:
-            cursor.execute(call_staff_turnover_query)
+            if not result[0]['count']:
+                cursor.execute(call_staff_turnover_query)
 
-        cursor.execute(get_staff_turnover_records_query)
-        records = make_dict_list_from_rows(cursor)
+            cursor.execute(get_staff_turnover_records_query)
+            records = make_dict_list_from_rows(cursor)
 
-        return render_template('report_result.html',
-                               records=records,
-                               year=year)
+            return render_template('report_result.html',
+                                   records=records,
+                                   year=year)
+    except DBConnectionError as e:
+        return 'Произошла ошибка соединения'
+    except DBCredentialError as e:
+        return 'Не удается войти в бд'
+    except DBBaseError as e:
+        return 'Произошла непредвиденная ошибка бд'
+    except DBSQLError as e:
+        return 'Ошибка базы данных'
+
